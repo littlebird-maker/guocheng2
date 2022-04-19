be7809acc47c6114467ab2b332205dae35ddd466
littlebird-maker <2528357780@qq.com>
2022-04-19 08:47:21 +0800

    First unit teset and url mapping,dummy view

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,15 @@
-from django.test import TestCase
+#from django.test import TestCase
 
-class SmokeTest(TestCase):
-    def test_bad_maths(self):
-        self.assertEqual(1+1,3)
-# Create your tests here.
+#class SmokeTest(TestCase):
+#    def test_bad_maths(self):
+#        self.assertEqual(1+1,3)
+## Create your tests here.
+
+from django.urls import resolve
+from django.test import TestCase
+from lists.views import home_page
+
+class HomePageTest(TestCase):
+    def test_root_url_resolve_to_home_page_view(self):
+        found=resolve('/')
+        self.assertEqual(found.func,home_page)
